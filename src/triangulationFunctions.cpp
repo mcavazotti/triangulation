109c83febdecd0f4aa511ce0d0a04a2b0f9bcfb5
Matheus Cavazotti <mcavazotti@gmail.com>
2021-06-22 21:52:46 -0300

    debugging...

--- src/triangulationFunctions.cpp
+++ src/triangulationFunctions.cpp
@@ -1,7 +1,48 @@
 #include "../include/triangulationFunctions.hpp"
 
+#ifdef DEBUG
+#include <stdio.h>
+#endif
+
+void setFace(HalfEdge *edgeChain, Face *face)
+{
+#ifdef DEBUG
+  fprintf(stderr, "\n\n#######\n");
+  fprintf(stderr, "setFace\n");
+  fprintf(stderr, "#######\n");
+#endif
+  HalfEdge *tmp, *highest;
+
+  tmp = edgeChain;
+  highest = edgeChain;
+
+#ifdef DEBUG
+  fprintf(stderr, "%d %d [%lx]\n", tmp->from()->x, tmp->from()->y, (long int)tmp);
+#endif
+  do
+  {
+    tmp->setFace(face);
+    if ((*tmp) > (*highest))
+      highest = tmp;
+
+    tmp = tmp->next();
+#ifdef DEBUG
+    fprintf(stderr, "%d %d [%lx]\n", tmp->from()->x, tmp->from()->y, (long int)tmp);
+#endif
+  } while (tmp != edgeChain);
+  if (face != nullptr)
+    face->setChain(highest);
+}
+
 void insertDiagonal(HalfEdge *fromEdge, HalfEdge *toEdge)
 {
+#ifdef DEBUG
+  fprintf(stderr, "\n#############\n");
+  fprintf(stderr, "insertDiagonal\n");
+  fprintf(stderr, "#############\n");
+  fprintf(stderr, "%d %d [%lx]\n", fromEdge->from()->x, fromEdge->from()->y, (long int)fromEdge);
+  fprintf(stderr, "%d %d [%lx]\n", toEdge->from()->x, toEdge->from()->y, (long int)toEdge);
+#endif
   auto diagonal = new HalfEdge(fromEdge->from(), toEdge->from(), fromEdge->twin(), toEdge->prev()->twin(), nullptr);
 #ifdef DEBUG
   diagonal->insertedAfter = 1;
@@ -16,23 +57,7 @@ void insertDiagonal(HalfEdge *fromEdge, HalfEdge *toEdge)
   diagonal->setTwin(diagonalTwin);
   toEdge->twin()->setNext(diagonalTwin);
   fromEdge->prev()->twin()->setPrev(diagonalTwin);
-}
 
-void setFace(HalfEdge *edgeChain, Face *face)
-{
-  HalfEdge *tmp, *highest;
-
-  tmp = edgeChain;
-  highest = edgeChain;
-
-  do
-  {
-    tmp->setFace(face);
-    if ((*tmp) > (*highest))
-      highest = tmp;
-
-    tmp = tmp->next();
-  } while (tmp != edgeChain);
-  if (face != nullptr)
-    face->setChain(highest);
+  setFace(diagonal, fromEdge->face());
+  setFace(diagonalTwin, new Face());
 }
